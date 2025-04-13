851c057649965950f85d31de045b9baa27effab0
DimashShayakhmetov <dimashshayakhmetov3@gmail.com>
2025-04-13 17:20:23 +0500

    added iterator support for MyArrayList for use in the foreach loop

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         MyQueue<Integer> queue = new MyQueue<>();
         queue.add(1);
-        queue.add(2);
+        
 
 
     }
